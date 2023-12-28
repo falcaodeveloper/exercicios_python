405a84e14b307ec4d130ede89d7a0e4889e86cb0
Ulisses Falcão <ulisses.falcao@outlook.com>
2023-12-28 16:23:49 -0300

    Create exercicio6.py

---
+++ exercicios/exercicio6.py
@@ -0,0 +1,8 @@
+#Impressão dos números de 1 a 10: Crie um programa que imprima os números de 1 a 10: https://horadecodar.com.br/exercicios-de-logica-de-programacao/
+#Desenvolvido por Ulisses F Falcão em 29/11/2023
+
+cont = 1
+
+while cont <= 10:
+    print(cont)
+    cont += 1
