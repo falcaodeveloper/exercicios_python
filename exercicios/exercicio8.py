b45329f85f291f505332e9c560be13386de8a50f
Ulisses Falcão <ulisses.falcao@outlook.com>
2023-12-30 14:34:55 -0300

    Create exercicio8.py

---
+++ exercicios/exercicio8.py
@@ -0,0 +1,8 @@
+#Contagem regressiva: Crie um programa que faça uma contagem regressiva de 10 até 1: https://horadecodar.com.br/exercicios-de-logica-de-programacao/
+#Desenvolvido por Ulisses F Falcão em 29/11/2023
+
+cont = 10
+
+while cont >= 1:
+    print(cont)
+    cont -= 1
