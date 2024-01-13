de1f7b2dceabb93d7e79dd59a38e1249414cf37e
falcaodeveloper <ulisses.falcao@outlook.com>
2024-01-13 19:38:48 -0300

    Aspas duplas

--- exercicios/exercicio23.py
+++ exercicios/exercicio23.py
@@ -1,7 +1,7 @@
 #Faça um programa que leia uma string do teclado e diga se ela é palíndromo. Uma string é palíndromo quando pode ser lida tanto de trás pra frente quanto de frente para trás e possui exatamente a mesma sequência de caracteres. Considere no desenvolvimento apenas palavras palíndromos: https://www.respondeai.com.br/conteudo/strings/exercicios/faca-programa-leia-string-teclado-diga-palindromo-string-palindromo-pode-10903
 
 #https://www.dicio.com.br/lista-palindromos/
-#Palavras com aspas ("") não funcionam: Aí, Lima falou: “Olá, família”.
+#Palavras com aspas duplas podem não funcionar, dependendo da fonte. Se copiar de algum lugar e depois colar, o resultado poderá ser errado. Exemplo: Aí, Lima falou: “Olá, família”.
 
 string = input('Digite uma palavra ou frase:\n').strip().lower()
 string = string.replace(' ', '')
@@ -13,6 +13,7 @@ string = string.replace('.', '')
 string = string.replace(',', '')
 string = string.replace('-', '')
 string = string.replace('_', '')
+string = string.replace('"', "")
 string = string.replace('í', 'i')
 string = string.replace('á', 'a')
 string = string.replace('ã', 'a')
