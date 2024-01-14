6e6ff4806a4e2bbe4c32047ce7c06542b5e4ec0f
falcaodeveloper <ulisses.falcao@outlook.com>
2024-01-14 11:37:36 -0300

    Exercício 24.py

---
+++ exercicios/exercicio24.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia duas strings. Após isso, o programa deve concatenar as informações lidas e mostrar o resultado para o usuário: https://www.respondeai.com.br/conteudo/strings/exercicios/faca-programa-leia-duas-strings-apos-programa-deve-concatenar-informacoes-10899
+
+informacao1 = input('Digite o que quiser:\n')
+informacao2 = input('Digite mais alguma coisa para concatenar com o que digitou anteriormente:\n')
+print('{} {}' .format(informacao1, informacao2))
