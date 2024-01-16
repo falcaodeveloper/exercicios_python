17f829298fd33c01629b2ec3770c0a84ce0067f4
falcaodeveloper <ulisses.falcao@outlook.com>
2024-01-16 12:51:21 -0300

    Exercício 26

---
+++ exercicios/exercicio26.py
@@ -0,0 +1,5 @@
+#Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo: https://www.computersciencemaster.com.br/exercicios-if-e-else/
+#Desenvolvido por Ulisses F. Falcão em 09/01/2024
+
+valor = float(input('Digite um valor qualquer: '))
+print('Valor negativo') if valor < 0 else print('Valor positivo')
